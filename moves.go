19f5e41d2287d87181066cb9983428a08d5486ff
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-12-01 06:17:38 -0500

    wrote a indexer for the working field, and started writing more rules

--- moves.go
+++ moves.go
@@ -1,7 +1,22 @@
 package sudoku
 
+type indexedCluster map[int]struct{
+	targets []int
+}
+
+func indexCluster(in []cell) (out indexedCluster) {
+	for id, each := range in {
+		for onePossible, _ := range each.possible {
+			tmp := out[onePossible]
+			tmp.targets = append(tmp.targets, id)
+			out[onePossible] = tmp
+		}
+	}
+	return out
+}
+
 // look at the possible values for a cell - if there's only one, mark that cell as solved
-func confirmCell(workingCell cell, c chan cell) bool {
+func confirmCell(workingCell cell, u chan cell) bool {
 	var firstFind int
 
 	for loc, each := range workingCell.possible {
@@ -14,7 +29,7 @@ func confirmCell(workingCell cell, c chan cell) bool {
 		}
 		if firstFind != 0 {
 			// send back an update and return true - you changed something
-			c <- cell{
+			u <- cell{
 				location: workingCell.location,
 				actual:   firstFind,
 			}
@@ -25,7 +40,7 @@ func confirmCell(workingCell cell, c chan cell) bool {
 }
 
 // Removes known values from other possibles in the same cluster
-func eliminatePossibles(workingCluster []cell, c chan cell) bool {
+func eliminatePossibles(workingCluster []cell, u chan cell) bool {
 	var solved map[int]bool
 	var remove map[int]bool
 	var changed bool
@@ -45,7 +60,7 @@ func eliminatePossibles(workingCluster []cell, c chan cell) bool {
 		}
 		if len(remove) > 0 {
 			// send back removal of possibles & reset
-			c <- cell{
+			u <- cell{
 				location: each.location,
 				possible: remove,
 			}
@@ -53,4 +68,35 @@ func eliminatePossibles(workingCluster []cell, c chan cell) bool {
 		}
 	}
 	return changed
+}
+
+func confirmIndexed(index indexedCluster, workingCluster []cell, u chan cell) bool {
+	var changed bool
+	for val, section := range index{
+		if len(section.targets) < 1{
+			// something went terribly wrong here
+		} else if len(section.targets) == 1 {
+			u <- cell{
+				location: workingCluster[section.targets[0]].location,
+				actual: val,
+			}
+			changed = true
+		}
+	}
+	return changed
+}
+
+func findPairs(index indexedCluster, workingCluster []cell, u chan cell) (changed bool) {
+	searchCount := 2
+
+	for searchCount < len(index) {
+		for val, valIndex := range localIndex {
+			if len(valIndex) <= searchCount {
+				localIndex := index
+				// this could be a first in a pair
+				// probalby use something recursive to find the second match? idk
+
+			}
+		}
+	}
 }
