8c428fbdbe21df6686ba746e3144adf511b7745e
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-12-25 05:54:11 -0500

    all tests for array.go except `TestSubArr` work

--- array.go
+++ array.go
@@ -4,6 +4,8 @@ import (
 	"sort"
 )
 
+// Holds some functions needed for working with arrays
+
 func dedupArr(arr []int) []int {
 	localArr := make([]int, len(arr))
 	copy(localArr, arr)
@@ -13,15 +15,13 @@ func dedupArr(arr []int) []int {
 			localArr = append(localArr[:i], localArr[i+1:]...)
 		}
 	}
-	if localArr[len(localArr)-2] == localArr[len(localArr)-1] {
+	if len(localArr) > 1 && localArr[len(localArr)-2] == localArr[len(localArr)-1] {
 		localArr = localArr[:len(localArr)-1]
 	}
 	return localArr
 }
 
-// Holds some functions needed for working with arrays
-
-func inArr(val int, arr []int) bool {
+func inArr(arr []int, val int) bool {
 	for _, each := range arr {
 		if each == val {
 			return true
@@ -30,7 +30,7 @@ func inArr(val int, arr []int) bool {
 	return false
 }
 
-func anyInArr(tgt, arr []int) bool {
+func anyInArr(arr, tgt []int) bool {
 	for _, t := range tgt {
 		for _, a := range arr {
 			if t == a {
@@ -41,42 +41,45 @@ func anyInArr(tgt, arr []int) bool {
 	return false
 }
 
-func addArr(add, arr []int) []int {
-	add = dedupArr(add)
-	arr = dedupArr(arr)
-	var i, j int
-	for i < len(add) && j < len(arr)-1 {
-		if add[i] == arr[j] {
-			j++
-		} else if add[i] < arr[j] {
-			i++
-		} else if add[i] > arr[i] {
-			arr = append(arr[:j], append(add[i:i], arr[j:]...)...)
-			j++
-		}
-	}
-	arr = dedupArr(arr)
-	return arr
+// preforms a union of a and b and removes anhy duplicates
+func addArr(a, b []int) []int {
+	return dedupArr(append(a, b...))
+	// a = dedupArr(a)
+	// b = dedupArr(b)
+	// var i, j int
+	// for i < len(a) && j < len(b)-1 {
+	// 	if a[i] == b[j] {
+	// 		j++
+	// 	} else if a[i] < b[j] {
+	// 		i++
+	// 	} else if a[i] > b[i] {
+	// 		b = append(b[:j], append(a[i:i], b[j:]...)...)
+	// 		j++
+	// 	}
+	// }
+	// b = dedupArr(b)
+	// return b
 }
 
-func subArr(sub, arr []int) []int {
-	sub = dedupArr(sub)
-	arr = dedupArr(arr)
+// Subtracts b from a - removing any intersections from a and returning
+func subArr(a, b []int) []int {
+	b = dedupArr(b)
+	a = dedupArr(a)
 	var i, j int
-	for i < len(sub) && j < len(arr)-1 {
-		if sub[i] == arr[j] {
-			arr = append(arr[:j], arr[:j+1]...)
-		} else if sub[i] < arr[j] {
+	for i < len(b) && j < len(a)-1 {
+		if b[i] == a[j] {
+			a = append(a[:j], a[:j+1]...)
+		} else if b[i] < a[j] {
 			i++
 		} else {
 			j++
 		}
 	}
-	return arr
+	return a
 }
 
 func andArr(a, b []int) []int {
-	var output []int
+	output := []int{}
 	a = dedupArr(a)
 	b = dedupArr(b)
 	var i, j int
--- array_test.go
+++ array_test.go
@@ -1,6 +1,7 @@
 package sudoku
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -22,6 +23,9 @@ func TestDedupArr(t *testing.T) {
 		}, {
 			[]int{2, 1, 8, 4, 16, 6, 16, 1, 4},
 			[]int{1, 2, 4, 6, 8, 16},
+		}, {
+			[]int{},
+			[]int{},
 		},
 	}
 
@@ -32,5 +36,186 @@ func TestDedupArr(t *testing.T) {
 
 		assert.Equal(t, testRun.in, input, "input changed")
 		assert.Equal(t, testRun.out, output, "output was incorrect")
+	}
+}
+
+func ExampleDedupArr() {
+	input := []int{2, 1, 8, 4, 16, 6, 16, 1, 4}
+	fmt.Println(dedupArr(input))
+
+	// Output:
+	// [1 2 4 6 8 16]
+}
+
+func TestInArr(t *testing.T) {
+	var tests = []struct {
+		in    []int
+		check int
+		out   bool
+	}{
+		{
+			[]int{1, 2, 3, 4, 5, 6},
+			3, true,
+		}, {
+			[]int{1, 1, 3, 4, 4, 4},
+			7, false,
+		},
+	}
+
+	for _, testRun := range tests {
+		result := inArr(testRun.in, testRun.check)
+		assert.Equal(t, testRun.out, result, "wrong response - expected %d", testRun.check)
+	}
+}
+
+func TestAnyInArr(t *testing.T) {
+	var tests = []struct {
+		in    []int
+		check []int
+		out   bool
+	}{
+		{
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+			true,
+		}, {
+			[]int{1, 1, 3, 4, 4, 4},
+			[]int{1},
+			true,
+		}, {
+			[]int{1, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+			true,
+		}, {
+			[]int{1, 1, 3, 4, 4, 4},
+			[]int{7},
+			false,
+		}, {
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+			true,
+		}, {
+			[]int{},
+			[]int{1, 2, 3, 4, 5, 6},
+			false,
+		}, {
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{},
+			false,
+		},
+	}
+
+	for id, testRun := range tests {
+		result := anyInArr(testRun.in, testRun.check)
+		assert.Equal(t, testRun.out, result, "wrong response - %d test", id)
+	}
+}
+
+func TestAddArr(t *testing.T) {
+	var tests = []struct {
+		a   []int
+		b   []int
+		out []int
+	}{
+		{
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+		}, {
+			[]int{1, 2, 3},
+			[]int{4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+		}, {
+			[]int{1, 2, 3, 4, 6},
+			[]int{1, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+		}, {
+			[]int{},
+			[]int{1, 4, 5, 6},
+			[]int{1, 4, 5, 6},
+		}, {
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{},
+			[]int{1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for id, testRun := range tests {
+		result := addArr(testRun.a, testRun.b)
+		assert.Equal(t, testRun.out, result, "wrong response test %d", id)
+	}
+}
+
+func TestSubArr(t *testing.T) {
+	var tests = []struct {
+		a   []int
+		b   []int
+		out []int
+	}{
+		{
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{},
+		}, {
+			[]int{1, 2, 3},
+			[]int{4, 5, 6},
+			[]int{1, 2, 3},
+		}, {
+			[]int{1, 2, 3, 4, 6},
+			[]int{1, 4, 5, 6},
+			[]int{2, 3},
+		}, {
+			[]int{},
+			[]int{1, 4, 5, 6},
+			[]int{},
+		}, {
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{},
+			[]int{1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for id, testRun := range tests {
+		result := subArr(testRun.a, testRun.b)
+		assert.Equal(t, testRun.out, result, "wrong response test %d", id)
+	}
+}
+
+func TestAndArr(t *testing.T) {
+	var tests = []struct {
+		a   []int
+		b   []int
+		out []int
+	}{
+		{
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6},
+		}, {
+			[]int{1, 2, 3},
+			[]int{4, 5, 6},
+			[]int{},
+		}, {
+			[]int{1, 2, 3, 7},
+			[]int{4, 5, 6, 7},
+			[]int{7},
+		}, {
+			[]int{1, 2, 3, 4, 6},
+			[]int{1, 4, 5, 6},
+			[]int{1, 4, 6},
+		}, {
+			[]int{},
+			[]int{1, 4, 5, 6},
+			[]int{},
+		}, {
+			[]int{1, 2, 3, 4, 5, 6},
+			[]int{},
+			[]int{},
+		},
+	}
+
+	for id, testRun := range tests {
+		result := andArr(testRun.a, testRun.b)
+		assert.Equal(t, testRun.out, result, "wrong response test %d", id)
 	}
 }
