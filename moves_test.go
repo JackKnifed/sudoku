5ed9e3458f867cee28acf7e786aa01369406daa0
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-12-27 17:40:24 -0500

    wrote initial test function to test the index clustering stuff

--- moves_test.go
+++ moves_test.go
@@ -20,4 +20,25 @@ func loadCellCluster(inputFile string) ([][]cell, error) {
 	return v
 }
 
-func Test
+func TestIndexCluster(t *testing.T) {
+	input, err := loadCellCluster("testdata/moves_input.json")
+	if err != nil {
+		t.FatalF("expected input could not be loaded - %v", err)
+	}
+
+	expected, err := loadCellCluster("testdata/moves_index.json")
+	if err != nil {
+		t.FatalF("expected results cound not be loaded - %v", err)
+	}
+
+	if len(input) != len(expected) {
+		t.FatalF("input and expected do not have the same number of tests")
+	}
+
+	for i := 0; i < len(input); i++ {
+		output := indexCluster(input[i])
+		sort.Sort(expected)
+		sort.Sort(output)
+		assert.Equal(t, expected, output, "expected index and returned index differ")
+	}
+}
