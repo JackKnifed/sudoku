2e48779031620f9125eb531833db2707ba9de9f3
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-12-27 19:16:18 -0500

    added test for `clusterSolved` and moved testing data into folder

--- moves_test.go
+++ moves_test.go
@@ -6,13 +6,27 @@ import (
 	"testing"
 )
 
-func loadCellCluster(inputFile string) ([][]cell, error) {
+func loadCluster(inputFile string) ([][]cell, error) {
 	b, err := ioutil.ReadFile(inputFile)
 	if err != nil {
 		return err
 	}
 
-	var v interface{}
+	var v [][]cell
+	err = Unmarshal(b, v)
+	if err != nil {
+		return err
+	}
+	return v
+}
+
+func loadBools(inputFile string) ([]bool, error) {
+	b, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		return err
+	}
+
+	var v []bool
 	err = Unmarshal(b, v)
 	if err != nil {
 		return err
@@ -21,12 +35,12 @@ func loadCellCluster(inputFile string) ([][]cell, error) {
 }
 
 func TestIndexCluster(t *testing.T) {
-	input, err := loadCellCluster("testdata/moves_input.json")
+	input, err := loadCluster("testdata/moves_input.json")
 	if err != nil {
 		t.FatalF("expected input could not be loaded - %v", err)
 	}
 
-	expected, err := loadCellCluster("testdata/moves_index.json")
+	expected, err := loadCluster("testdata/moves_index.json")
 	if err != nil {
 		t.FatalF("expected results cound not be loaded - %v", err)
 	}
@@ -37,8 +51,36 @@ func TestIndexCluster(t *testing.T) {
 
 	for i := 0; i < len(input); i++ {
 		output := indexCluster(input[i])
-		sort.Sort(expected)
-		sort.Sort(output)
-		assert.Equal(t, expected, output, "expected index and returned index differ")
+		assert.Equal(t, expected, output, "test %d - expected index and returned index differ", i)
+	}
+}
+
+func TestClusterSolved(t *testing.T) {
+	input, err := loadCluster("testdata/moves_input.json")
+	if err != nil {
+		t.FatalF("expected input could not be loaded - %v", err)
+	}
+
+	expected, err := loadBools("testdata/moves_index.json")
+	if err != nil {
+		t.FatalF("expected results cound not be loaded - %v", err)
+	}
+
+	if len(input) != len(expected) {
+		t.FatalF("input and expected do not have the same number of tests")
+	}
+
+	for i := 0; i < len(input); i++ {
+		var update chan cell
+		var more bool
+		result := clusterSolved(input[i], update)
+		select {
+		case _, more = <-update:
+		default:
+			more = true
+		}
+
+		assert.Equal(t, expected[i], result, "test %d - return value differs", i)
+		assert.Equal(t, expected[i], more, "test %d - channel status differs", i)
 	}
 }
