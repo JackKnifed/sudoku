b7d2f0ea9741c49308ed253db37beadf4746f7a4
Jack Hayhurst <James.A.Hayhurst@gmail.com>
2015-12-28 23:36:51 -0500

    wrote test function for move number 2

--- moves_test.go
+++ moves_test.go
@@ -48,7 +48,7 @@ func loadIndex(inputFile string) ([]bool, error) {
 	return v
 }
 
-func sortResponses(in, out chan cell) {
+func updateCatcher(in, out chan cell) {
 	defer close(out)
 	more := true
 	var updates Cluster
@@ -114,5 +114,51 @@ func TestClusterSolved(t *testing.T) {
 
 		assert.Equal(t, expected[i], result, "test %d - return value differs", i)
 		assert.Equal(t, expected[i], more, "test %d - channel status differs", i)
+	}
+}
+
+func TestSolvedNoPossible(t *testing.T) {
+	input, err := loadCluster("testdata/moves_input.json")
+	if err != nil {
+		t.FatalF("input could not be loaded - %v", err)
+	}
+
+	expectedResponse, err := loadCluster("testdata/moves_two_response.json")
+	if err != nil {
+		t.FatalF("expected cound not be loaded - %v", err)
+	}
+
+	expected, err := loadCluster("testdata/moves_two_updates.json")
+	if err != nil {
+		t.FatalF("expected cound not be loaded - %v", err)
+	}
+
+	if len(input) != len(expected) {
+		t.FatalF("input and expected count differs")
+	}
+
+	for i := 0; i < len(input); i++ {
+		var updates, back chan cell
+		var more bool
+		go updateCatcher(updates, back)
+		result := clusterSolved(input[i], update)
+
+		assert.Equal(t, expectedResponse[i], result, "expected and actual return differs")
+		close(updates)
+		sort.Sort(expected[i])
+
+		for _, update := range expected[i] {
+			assert.Equal(t, expected[i], <-back, "test %d - return value differs", i)
+		}
+
+		junk, more := <-update
+		if more {
+			t.Fail()
+			t.Logf("test %d - channel status differs", i)
+			for more {
+				t.Log(junk)
+				junk, more = <-update
+			}
+		}
 	}
 }
